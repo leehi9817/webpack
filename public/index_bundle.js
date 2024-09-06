c171480e83a0cda1112df76575912674f2c48bc6
leehi9817 <leehi9817@gmail.com>
2024-09-06 14:55:55 +0900

    first commit

---
+++ public/index_bundle.js
@@ -0,0 +1,4 @@
+(() => {
+  "use strict";
+  $("#root").html("Hello World");
+})();
---
+++ source/index.js
@@ -0,0 +1,3 @@
+import hello_word from "./hello.js";
+import world_word from "./world.js";
+$("#root").html(hello_word + " " + world_word);
