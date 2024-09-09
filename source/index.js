3aa3d5acaa22470999731775a72ae2c83df76cef
leehi9817 <leehi9817@gmail.com>
2024-09-09 09:21:32 +0900

    style-loader, css-loader 적용

--- source/index.js
+++ source/index.js
@@ -1,3 +1,5 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
+import css from "./style.css";
 $("#root").html(hello_word + " " + world_word);
+console.log("css", css);
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,15 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     filename: "index_bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", // 가져온 파일을 webpage안에 style 태그로 주입해주는 로더
+          "css-loader", // css 파일을 읽어와서 webpack으로 가져오는 로더
+        ],
+      },
+    ],
+  },
 };
