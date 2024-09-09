a09c61b65063e21e3d5731ef37a1c195700fa6ed
leehi9817 <leehi9817@gmail.com>
2024-09-09 10:52:06 +0900

    npm 패키지 (lodash) 사용

--- source/about.js
+++ source/about.js
@@ -1,6 +1,7 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
 import $ from "jquery";
+import _ from "lodash";
 import css from "./style.css";
 
-$("#root").html(world_word + " " + hello_word);
+$("#root").html(_.join([world_word, hello_word], " "));
--- source/index.js
+++ source/index.js
@@ -1,6 +1,7 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
 import $ from "jquery";
+import _ from "lodash";
 import css from "./style.css";
 
-$("#root").html(hello_word + " " + world_word);
+$("#root").html(_.join([hello_word, world_word], " "));
